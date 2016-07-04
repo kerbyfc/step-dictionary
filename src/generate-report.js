c8852a9c8181d96f1c31f7042c01cb1959add2f0
kerbyfc <kerbyfc@gmail.com>
2016-07-04 22:11:23 +0300

    parse .ts files instead
    for ts-flow usage

--- src/generate-report.js
+++ src/generate-report.js
@@ -1,192 +1,101 @@
-import _ from 'lodash';
-import path from 'path';
-import modulePackage from '../package.json';
+'use strict';
 
-export default function(stepDefinitions) {
-  let groupedStepDefintions = _.groupBy(stepDefinitions, 'file');
-  let title = 'Step Dictionary';
-  let cssFiles = ['https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css'];
-  let jsImports = ['https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js'];
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-  let cssBlock = cssFiles.map((location) => {
-    return `<link rel="stylesheet" type="text/css" href="${location}">`;
-  }).join('\n');
+var _path = require('path');
 
-  let jsBlock = jsImports.map((location) => {
-    return `<script src="${location}"></script>`;
-  }).join('\n');
+var _path2 = _interopRequireDefault(_path);
 
-  let inlineCSS = `
-    #filter {
-      width: 100%;
-      font-size: 18pt;
-      padding: 10px;
-      background-color: #F0F0F0;
-      border: 1px solid #CCC;
-      color: #0088B5;
-    }
-    .filename-header {
-      font-size: 13pt;
-    }
-    .step-defintion {
-      padding: 20px 5px;
-    }
-    .label {
-      color: #000;
-    }
-    .form-group, .controls, .footer {
-      padding: 20px 0;
-    }
-    .fileToggleButton {
-      float: left;
-      position: relative;
-      margin-right: 10px;
-      border: 1px solid #CCC;
-      background-color: #FFF;
-      font-size: 8pt;
-      color: #AAA;
-    }
-    .fileToggleButton:hover {
-      border: 1px solid #777;
-      color: #333;
-    }
-    .footer {
-      font-size: 10pt;
-    }
-  `;
+var _lodash = require('lodash');
 
-  let header = `
-    <h1> Step Dictionary </h1>
-    <div class="form-group">
-      <label for="filter">Search by phrase</label>
-      <input type="text" id="filter" placeholder="">
-    </div>
-  `;
+var _lodash2 = _interopRequireDefault(_lodash);
 
-  let filesBlock = '';
-  _.forIn(groupedStepDefintions, (steps, file) => {
-    let stepsBlock = steps.map((step) => {
-      return `
-        <dl class="dl-horizontal step-definition" regex="${encodeURIComponent(step.regex)}">
-          <dt>Regex</dt>
-          <dd>${step.regex}</dd>
-          <dt>Keyword</dt>
-          <dd>${step.keyword}</dd>
-          <dt>Parameters</dt>
-          <dd>${step.params}</dd>
-          <dt>URI</dt>
-          <dd>${path.basename(file)}:${step.line}</dd>
-        </dl>
-      `;
-    }).join(' ');
-    filesBlock += `
-      <div class="file-block">
-        <input type="button" class="fileToggleButton" onClick="toggleDefinitions(this)" value="Toggle">
-        <p class="filename-header">
-          ${path.relative(process.cwd(), file)}
-        </p>
-        <div class="definitions">
-          ${stepsBlock}
-        </div>
-      </div>
-    `;
-  });
+var _fs = require('fs');
 
-  let filterScript = `
-    <script>
-      $('#filter').bind('propertychange change click keyup input paste', function(e) {
-        var newValue = e.currentTarget.value;
-        $('.step-definition').each(function(elem) {
-          var regexString = decodeURIComponent(this.getAttribute('regex'));
-          var searchFilter = newValue.replace(/ +/g, \'.*\').replace(/\".*\"/, '\".*\"');
-          var matchDefinition = new RegExp(searchFilter);
-          if (!matchDefinition.test(regexString)) {
-            this.style.display = 'none';
-          } else {
-            this.style.display = 'block';
-          }
-        });
+var _fs2 = _interopRequireDefault(_fs);
 
-        $('.file-block').each(function() {
-          var noneFound = true;
-          $(this).find('.step-definition').each(function() {
-            var regexString = decodeURIComponent(this.getAttribute('regex'));
-            var searchFilter = newValue.replace(/ +/g, \'.*\').replace(/\".*\"/, '\".*\"');
-            var matchDefinition = new RegExp(searchFilter);
-            if (!matchDefinition.test(regexString)) {
-              this.style.display = 'none';
-            } else {
-              noneFound = false;
-              this.style.display = 'block';
-            }
-          });
-          console.log('nf: ' + noneFound);
-          if (noneFound) {
-            this.style.display = 'none';
-          } else {
-            this.style.display = 'block';
-          }
-        });
+var _glob = require('glob');
+
+var _glob2 = _interopRequireDefault(_glob);
+
+var _generateReport = require('./generate-report');
+
+var _generateReport2 = _interopRequireDefault(_generateReport);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+module.exports = function () {
+  function StepDictionary(pathArg) {
+    _classCallCheck(this, StepDictionary);
+
+    this.paths = this._getAllPaths(pathArg);
+    this.stepDefinitions = this._getStepDefinitions();
+  }
+
+  _createClass(StepDictionary, [{
+    key: 'getStepsJson',
+    value: function getStepsJson() {
+      return this.stepDefinitions;
+    }
+  }, {
+    key: 'getStepThatMatches',
+    value: function getStepThatMatches(phrase) {
+      return this.stepDefinitions.filter(function (stepDef) {
+        return stepDef.regex.test(phrase);
       });
-    </script>
-  `;
+    }
+  }, {
+    key: 'outputReport',
+    value: function outputReport(outputPath) {
+      var report = (0, _generateReport2.default)(this.stepDefinitions);
+      _fs2.default.writeFileSync(outputPath, report);
+    }
+  }, {
+    key: '_getStepDefinitions',
+    value: function _getStepDefinitions() {
+      var stepDefinitions = [];
 
-  let toggleScripts = `
-    <script>
-      function toggleDefinitions(elem) {
-        var definitions = $(elem).siblings()[1];
-        $(definitions).toggle();
-      }
+      this.paths.forEach(function (filePath) {
+        var fileData = void 0;
 
-      function toggleAll(action) {
-        if (action === 'collapse') {
-          $('.definitions').css('display', 'none');
-          $('#toggleAll').text('Show All');
-          $('#toggleAll').attr("href", "javascript:toggleAll('show')");
-        } else {
-          $('.definitions').css('display', 'block');
-          $('#toggleAll').text('Collapse All');
-          $('#toggleAll').attr("href", "javascript:toggleAll('collapse')");
+        try {
+          fileData = _fs2.default.readFileSync(filePath, { encoding: 'utf8' });
+        } catch (e) {
+          console.log(_path2.default + ' could not be read, will skip and continue', e);
         }
-      }
-    </script>
-  `;
 
-  let mainBody = `
-    <div class="controls">
-      Available step definitions (${stepDefinitions.length}):
-      <a href="javascript:toggleAll('collapse')" id="toggleAll"> Collapse All </a>
-    </div>
-    ${filesBlock}
-    ${filterScript}
-    ${toggleScripts}
-  `;
+        if (fileData) {
+          var re = /@(given|then)\(\s*\/\^(.*)\$\/\).*\n*.*\((.*)\)/g,
+              definition;
+          while ((definition = re.exec(fileData)) !== null) {
+            stepDefinitions.push({
+              keyword: definition[1],
+              regex: definition[2],
+              params: definition[3],
+              file: filePath,
+              line: fileData.slice(0, definition.index).split(/\n/).length
+            });
+          }
+        }
+      });
 
-  let footer = `
-    <div class="footer">
-      This report was generated by the template defined in step-dictionary@${modulePackage.version}
-    </div>
-  `;
+      return stepDefinitions;
+    }
+  }, {
+    key: '_getAllPaths',
+    value: function _getAllPaths(pathArg) {
+      var paths = typeof pathArg === 'string' ? [pathArg] : pathArg;
+      return _lodash2.default.flattenDeep(paths.map(function (filePath) {
+        if (_path2.default.parse(filePath).ext) {
+          return _path2.default.resolve(filePath);
+        } else {
+          return _glob2.default.sync(_path2.default.join(filePath, '**', '*.ts'));
+        }
+      }));
+    }
+  }]);
 
-  return `
-    <html>
-      <head>
-        <title>
-          ${title}
-        </title>
-        ${cssBlock}
-        ${jsBlock}
-        <style>
-          ${inlineCSS}
-        </style>
-      </head>
-      <body>
-        <div class="container">
-          ${header}
-          ${mainBody}
-          ${footer}
-        </div>
-      </body>
-    </html>
-  `;
-}
+  return StepDictionary;
+}();
